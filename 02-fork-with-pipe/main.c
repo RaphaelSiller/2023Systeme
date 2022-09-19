862f34d09e4059137230013bd38a1e99effc54d8
RaphaelSiller <raphael.siller123@gmail.com>
2022-09-19 11:17:29 +0200

    refactor<02>: added more comments

--- 02-fork-with-pipe/main.c
+++ 02-fork-with-pipe/main.c
@@ -6,33 +6,44 @@
 int main() {
   int result = 0;
   int fildes[2];
-  int pid;
 
+  // Erstellen der Pipe
   if (pipe(fildes) == -1) {
     printf("Fehler beim Pipe legen");
     return -1;
   }
 
+  // Hier wird geforkt
   switch (fork()) {
   case -1:
     printf("Fehler beim forken");
     return -1;
 
-  case 0: //Kind schreibt "Hallo Welt"
-    close(fildes[0]); // Schließe unbenutztes Read
-    result = 6-4;
-    
+  case 0: // Kind schreibt "Hallo Welt"
+    // Schließe unbenutztes Read
+    close(fildes[0]);
+
+    // Berechne Teil vom Kind
+    result = 6 - 4;
+
+    // Schreibe Ergebnis zurück zum Parent Process
     write(fildes[1], &result, sizeof result);
+
     return 0;
 
-  default: //Elternprozess
-    close(fildes[1]); //Schließe unbenutztes Write
-    result=3+1;
+  default: // Elternprozess
+    // Schließe unbenutztes Write
+    close(fildes[1]);
+
+    // Berechne Teil vom Elt.
+    result = 3 + 1;
+
+    // Bekomme Ergebnis vom Kind
     int resultFromChild;
     wait(NULL);
-    
     read(fildes[0], &resultFromChild, sizeof resultFromChild);
 
+    // Ausgeben des Ergebnis
     printf("Ergebnis von x = (3 + 1) * (6 – 4) = %i\n", (result * resultFromChild));
     printf("Ergebnis von Kind (6 - 4) war %i\n", resultFromChild);
     printf("Ergebnis von Elt. (3 + 1) war %i\n", result);
