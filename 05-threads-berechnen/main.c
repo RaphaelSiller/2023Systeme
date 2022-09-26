da7c4754af8d1bdabfcbe09aea93d3a323a46984
RaphaelSiller <raphael.siller123@gmail.com>
2022-09-26 12:33:37 +0200

    fix<05>: added comments

--- 05-threads-berechnen/main.c
+++ 05-threads-berechnen/main.c
@@ -14,6 +14,7 @@
 void *funcThread(void *arg) {
   /* int ret = *((int *)arg);
   ret*=2; */
+  //Return Calculated Result and exit Thread
   pthread_exit((*((int *)arg)) * 2);
 }
 
@@ -22,18 +23,19 @@ int main() {
 
 	int x = 4, ergebnis;
 
+  //Create Thread
   if(pthread_create(&secondThread, NULL, funcThread, (void *) &x) != 0) {
     printf("Fehler beim erstellen des Threads");
     exit(1);
   }
 
+  //Join Thread (Wait for Thread and save return value)
   if(pthread_join(secondThread, &ergebnis) != 0) {
     printf("Fehler beim Thread join");
     exit(2);
-  }
+  }  
 
-  
-
+  //Print Result
   printf("x= %i\n", x);
   printf("ergebnis= %i\n", ergebnis);
   return 0;
