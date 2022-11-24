f57b5a2b20e49e84d6f49256e637c324e0ffa18a
RaphaelSiller <raphael.siller123@gmail.com>
2022-11-24 20:24:57 +0100

    fix<crc>: improved integration of cmd options

--- 09-crc/crc.h
+++ 09-crc/crc.h
@@ -1,3 +1,3 @@
 void verschluesseln(long msg, long gen);
-void entschluesseln(long msg, long gen, long checksum);
+int entschluesseln(long msg, long gen, long checksum);
 int getNumberOfDigits(int n);
